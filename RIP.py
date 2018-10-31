bfe015b0c6e96b9f25b8348c92d453f8802a821f
Bruno Morii <brunomorii.1998@gmail.com>
2018-10-30 23:16:11 -0300

    Melhoras nas impressões

--- RIP.py
+++ RIP.py
@@ -12,27 +12,38 @@ def tolayer2(rcvdpkt):
 
 
 def printaTabela(sourceid, mincost):
-	print ("Tabela do nó %d  \n0 1 2 3\n%d %d %d %d" % (sourceid, mincost[0], mincost[1], mincost[2], mincost[3]))
+	print ("\n**********Tabela do nó %d**********\n" %(sourceid))
+	print ("Para nó 0 | %d | Através de j" %(mincost[0]))
+	print ("Para nó 1 | %d | Através de j" %(mincost[1]))
+	print ("Para nó 2 | %d | Através de j" %(mincost[2]))
+	print ("Para nó 3 | %d | Através de j" %(mincost[3]))
+	print ("\n***********************************\n")
+
 
 class Rtpkt:
 	sourceid = -1 #identificador do nó
 	destid = -1 #identificador do nó destino
-	mincost = [100, 100, 100, 100] # tabela
+	mincost = [999, 999, 999, 999] # tabela
+
 	def __init__(self, sourceid, destid, mincost):
 		self.sourceid = sourceid
 		self.destid = destid
 		self.mincost = mincost
+
 	def get_sourceid(self):
 		return self.sourceid
+
 	def get_destid(self):
 		return self.destid
+
 	def get_mincost(self):
 		return self.mincost
 
 class Node:
 	sourceid = -1 # identificador do nó
 	neighborhood = [] # vetor vizinhança
-	mincost = [100, 100, 100, 100] # tabela
+	mincost = [999, 999, 999, 999] # tabela
+
 	def __init__(self, sourceid, neighborhood, mincost):
 		#inicialização
 		self.sourceid=sourceid
@@ -60,6 +71,7 @@ class Node:
 			if (mincost[x] + self.mincost[sourceid]) < self.mincost[x] and x != sourceid:
 				self.mincost[x] = mincost[x] + self.mincost[sourceid]
 				change = 1
+
 		#se teve alteração é mandado a tabela para os vizinhos
 		if change == 1:
 			time.sleep(random.random()*5)
@@ -69,22 +81,27 @@ class Node:
 def execute(sourceid, neighborhood, mincost):
 	
 	nodes[sourceid] = Node(sourceid, neighborhood, mincost)
+	printaTabela(sourceid, mincost)
 	time.sleep(random.random()*5)
 	nodes[sourceid].send_table()
 
 
 
-#rotina principal, adicionamento de threads
+#rotina principal, adicionando threads
 nodes = [Node(-1,[],[]), Node(-1,[],[]), Node(-1,[],[]), Node(-1,[],[])]
 t_0 = threading.Thread(target= execute, args = (0, [1, 2, 3], [0, 1, 3, 7]))
-t_1 = threading.Thread(target= execute, args = (1, [0, 2], [1, 0, 1, 100]))
+t_1 = threading.Thread(target= execute, args = (1, [0, 2], [1, 0, 1, 999]))
 t_2 = threading.Thread(target= execute, args = (2, [0, 1, 3], [3, 1, 0, 2]))
-t_3 = threading.Thread(target= execute, args = (3, [0, 2], [7, 100, 2, 0]))
+t_3 = threading.Thread(target= execute, args = (3, [0, 2], [7, 999, 2, 0]))
 
 t_0.start()
+time.sleep(0.1)
 t_1.start()
+time.sleep(0.1)
 t_2.start()
+time.sleep(0.1)
 t_3.start()
+time.sleep(0.1)
 t_0.join()
 t_1.join()
 t_2.join()
